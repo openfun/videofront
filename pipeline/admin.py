83e84f34ec44f49b72493188cd7dea66be614f91
Régis Behmo <regis@behmo.com>
2016-08-16 16:46:53 +0200

    Add playlists to admin site

--- pipeline/admin.py
+++ pipeline/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+
 class VideoTranscodingInlineAdmin(admin.TabularInline):
     model = models.VideoTranscoding
+
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('public_id', 'title', 'transcoding_progress', 'transcoding_status', 'transcoding_started_at')
@@ -13,9 +15,17 @@ class VideoAdmin(admin.ModelAdmin):
         qs = super(VideoAdmin, self).get_queryset(request)
         return qs.select_related('transcoding')
 
+
 class VideoUploadUrlAdmin(admin.ModelAdmin):
     model = models.VideoUploadUrl
     list_display = ('public_video_id', 'expires_at', 'was_used', 'last_checked')
 
+
+class PlaylistAdmin(admin.ModelAdmin):
+    model = models.Playlist
+    list_display = ('public_id', 'name', 'owner',)
+
+
 admin.site.register(models.Video, VideoAdmin)
 admin.site.register(models.VideoUploadUrl, VideoUploadUrlAdmin)
+admin.site.register(models.Playlist, PlaylistAdmin)
