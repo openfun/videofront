e58ebca565d05ce8b4aca4ec01532da83cca91e1
Régis Behmo <regis@behmo.com>
2016-08-16 17:23:37 +0200

    Display video owner in admin

--- pipeline/admin.py
+++ pipeline/admin.py
@@ -8,7 +8,10 @@ class VideoTranscodingInlineAdmin(admin.TabularInline):
 
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ('public_id', 'title', 'transcoding_progress', 'transcoding_status', 'transcoding_started_at')
+    list_display = (
+        'public_id', 'title', 'owner',
+        'transcoding_progress', 'transcoding_status', 'transcoding_started_at',
+    )
     inlines = [VideoTranscodingInlineAdmin]
 
     def get_queryset(self, request):
@@ -18,7 +21,7 @@ class VideoAdmin(admin.ModelAdmin):
 
 class VideoUploadUrlAdmin(admin.ModelAdmin):
     model = models.VideoUploadUrl
-    list_display = ('public_video_id', 'expires_at', 'was_used', 'last_checked')
+    list_display = ('public_video_id', 'owner', 'expires_at', 'was_used', 'last_checked',)
 
 
 class PlaylistAdmin(admin.ModelAdmin):
