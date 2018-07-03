848065c5b443426af8ca7825583d983cdf52e7fe
ProCAN Group <wajdi.louati@procan-group.com>
2018-07-03 12:38:00 +0100

    Update views.py

--- ltivideofront/views.py
+++ ltivideofront/views.py
@@ -11,14 +11,18 @@ app_name = 'LTIFRONT'
 logger = logging.getLogger(__name__)
 
 def index(request):
+    countV=0
+    videos=None
     id_xblock=request.session["id_xblock"]
     logger.info("////////id_xblock%s" % id_xblock)
     resourcelink=LTIResource.objects.filter(resource_link_id=id_xblock)
     if resourcelink.exists():
          logger.info("////////existe")
-         for playlist in resourcelink[0].playlists:
-                logger.info("////////playlistname" )
-    return render(request,'home.html', {})
+         for playlist in resourcelink[0].playlists.all():
+                logger.info("////////playlistname%s" % playlist.name )
+                videos=playlist.videos.all()
+                countV=videos.count() 
+    return render(request,'home.html', {"countV":countV,"videos":videos})
 
 class MyLTILaunchView(LTILaunchView):
     def hook_before_post(self, request):
@@ -36,4 +40,51 @@ class MyLTILaunchView(LTILaunchView):
 
     def hook_get_redirect(self):
         return super(MyLTILaunchView, self).hook_get_redirect()
+
+
+views.py
+
+from django.http import HttpResponsePermanentRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render,redirect,render_to_response
+import json
+from django_app_lti.views import LTILaunchView
+from django.contrib.auth.models import User
+import logging
+from django.template import RequestContext
+from django_app_lti.models import LTIResource
+app_name = 'LTIFRONT'
+logger = logging.getLogger(__name__)
+
+def index(request):
+    countV=0
+    videos=None
+    id_xblock=request.session["id_xblock"]
+    logger.info("////////id_xblock%s" % id_xblock)
+    resourcelink=LTIResource.objects.filter(resource_link_id=id_xblock)
+    if resourcelink.exists():
+         logger.info("////////existe")
+         for playlist in resourcelink[0].playlists.all():
+                logger.info("////////playlistname%s" % playlist.name )
+                videos=playlist.videos.all()
+                countV=videos.count() 
+    return render(request,'home.html', {"countV":countV,"videos":videos})
+
+class MyLTILaunchView(LTILaunchView):
+    def hook_before_post(self, request):
+        '''Called before models are created and initialized in hook_process_post().'''
+
+        pass
+
+    def hook_process_post(self, request):
+        '''Creates and initializes models.'''
+        super(MyLTILaunchView, self).hook_process_post(request)
+
+    def hook_after_post(self, request):
+        '''Called after models are initialized.'''
+        pass
+
+    def hook_get_redirect(self):
+        return super(MyLTILaunchView, self).hook_get_redirect()
+
 
