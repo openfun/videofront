d8b5e5abca3d33bca060db351a71e457f163ff1c
wajdi louati <wajdi.louati@procan-group.com>
2018-07-03 11:05:54 +0100

    Module modified

--- django_app_lti/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django_app_lti.models import LTICourse, LTICourseUser, LTIResource
-
-admin.site.register(LTICourse)
-admin.site.register(LTICourseUser)
-admin.site.register(LTIResource)
--- ims_lti_py/__init__.py
+++
@@ -1,9 +0,0 @@
-# Classes
-from . tool_config import ToolConfig
-from . tool_consumer import ToolConsumer
-from . tool_provider import ToolProvider
-from . outcome_request import OutcomeRequest
-from . outcome_response import OutcomeResponse
-
-# Exceptions
-from . utils import InvalidLTIConfigError, InvalidLTIRequestError
